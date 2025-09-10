c2d0f646cfa702a1f0f41e91f7b27d5180aed382
Abhirag <abhiragsv2005@gmail.com>
2025-09-11 00:04:43 +0530

    tutorial#2

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,47 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-
+import { Image, StyleSheet, Text, View } from 'react-native'
+import Logo from '../assets/img/logo.jpg'
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+        <Image source={Logo} style={styles.image}/>
+      <Text style={styles.title}>Home</Text>
+
+      {/* inline styling */}
+      <Text style={{ fontSize: 18,marginTop:10 }}>Reading List App</Text>
+
+        <View style={styles.card}>
+            <Text style={{ color:'white' }}>This is the second view</Text>
+        </View>
+
     </View>
+    
   )
 }
 
 export default Home
-
-const styles = StyleSheet.create({})
+//internal styling
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    title:{
+        fontSize:24,
+        fontWeight:'bold',
+        marginBottom:16
+    },
+    card:{
+        backgroundColor:'black',
+        padding:16,
+        borderRadius:8,
+    },
+    image:{
+        marginVertical:20,
+        width:100,
+        height:100,
+        borderRadius:50,
+        borderWidth:2,
+        borderColor:'blue'
+    }
+})
